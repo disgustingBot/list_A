736ac92696e274191e8f42cec14c73dc118b5151
molinerozadkiel <50532987+molinerozadkiel@users.noreply.github.com>
2019-06-25 06:56:14 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, user-scalable=no">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title>List-A</title>
+	<link rel="shortcut icon" href="img/logo.png" >
+	<meta       name="mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<!-- <link rel="manifest" href="/manifest.json"> -->
+
+	<!-- Normalize! -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+	<!-- Mis estilos -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+
+
+	<!-- <nav id="navBar">
+		<button id="mainMenuButton" onclick="alternateMainMenu()">
+			<div class="menuBar"></div>
+			<div class="menuBar"></div>
+			<div class="menuBar"></div></button>
+
+		<h2 id="title">list-A</h2>
+
+		<button id="elementMenu">
+			<div class="menuDot"></div>
+			<div class="menuDot"></div>
+			<div class="menuDot"></div></button>
+	</nav> -->
+
+
+
+
+	<section id="list" class="login">
+<!--
+		<form id="loginForm" action="">
+			<h2 id="loginTitle">Login</h2>
+			<input class="loginInput" id="loginInputLog" name="log" type="text"     placeholder="Nick/E-mail">
+			<input class="loginInput" id="loginInputPas" name="pwd" type="password" placeholder="Password"   >
+			<span  class="loginInput" id="loginInputButt" onclick="loginButton()">GO!</span>
+		</form>
+		<p id="errorMessage"></p>
+		<p id="joinUs" onclick="bringSignIn()">you don't have an account?</p>
+
+
+		<form id="signupForm" action="">
+			<h2 id="signUpTitle">New account</h2>
+			<input  class="loginInput" id="signUPInputNick" name="uid" type="text"     placeholder="Nick name"      >
+			<input  class="loginInput" id="signUPInputName" name="fst" type="text"     placeholder="First name"     >
+			<input  class="loginInput" id="signUPInputLast" name="lst" type="text"     placeholder="Last name"      >
+			<input  class="loginInput" id="signUPInputMail" name="eml" type="text"     placeholder="E-mail"         >
+			<input  class="loginInput" id="signUPInputPass" name="pwd" type="password" placeholder="Password"       >
+			<input  class="loginInput" id="signUPInputPas2" name="pw2" type="password" placeholder="Repeat password">
+			<span   class="loginInput" id="signUPInputButt" onclick="newUser()">NEW</span>
+		</form> -->
+		<h1>List-A</h1>
+
+		<button class="sign">sign up</button>
+		<button class="sign">sign in</button>
+	</section>
+
+
+	<script type="text/javascript" src="js/log.js?v=123983"></script>
+</body>
+</html>
